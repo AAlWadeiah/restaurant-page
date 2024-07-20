5bd72d9c33aa414e95f580fc2215c4a6dfcac244
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-07-20 11:26:21 -0600

    creating Hours div for homepage

--- src/homePage.js
+++ src/homePage.js
@@ -16,7 +16,32 @@ export default function loadHome(container) {
     rLi.textContent = r;
     recipeList.appendChild(rLi);
   }
-
   description.appendChild(recipeList);
   container.appendChild(description);
+
+  let hoursArr = [
+    "Sunday: 8am - 8pm",
+    "Monday: 6am - 6pm",
+    "Tuesday: 6am - 6pm",
+    "Wednesday: 6am - 6pm",
+    "Thursday: 6am - 10pm",
+    "Friday: 6am - 10pm",
+    "Saturday: 8am - 10pm",
+  ];
+
+  const hoursDiv = document.createElement("div");
+
+  const hoursTitle = document.createElement("h2");
+  hoursTitle.textContent = "Hours";
+  hoursDiv.appendChild(hoursTitle);
+
+  const hoursList = document.createElement("ul");
+
+  for (const hr of hoursArr) {
+    const hrLi = document.createElement("li");
+    hrLi.textContent = hr;
+    hoursList.appendChild(hrLi);
+  }
+  hoursDiv.appendChild(hoursList);
+  container.appendChild(hoursDiv);
 }
