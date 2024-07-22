91365b191dcdca434ef13e8107a3e57c29196d75
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-07-22 06:44:15 -0600

    Added basic elements to About and Contact page

--- src/aboutPage.js
+++ src/aboutPage.js
@@ -1,1 +1,10 @@
-export default function loadAbout(container) {}
+export default function loadAbout(container) {
+  const pageTitle = document.createElement("h1");
+  pageTitle.textContent = "About Us";
+  container.appendChild(pageTitle);
+
+  const desc = document.createElement("p");
+  desc.textContent =
+    "Our roots start in Palestine, Gaza ... Ut exercitation ipsum id cillum labore amet non amet elit est anim labore nisi. Nostrud deserunt deserunt adipisicing incididunt tempor aute ullamco proident ex eu mollit ad aliquip ipsum. Lorem in anim sit amet officia est sint ut irure. Cupidatat est aute est nulla dolore deserunt quis consectetur mollit quis reprehenderit elit ullamco.";
+  container.appendChild(desc);
+}
--- src/contactPage.js
+++ src/contactPage.js
@@ -1,1 +1,16 @@
-export default function loadContact(container) {}
+export default function loadContact(container) {
+  const pageTitle = document.createElement("h1");
+  pageTitle.textContent = "Contact Us";
+  container.appendChild(pageTitle);
+
+  const contactDiv = document.createElement("div");
+  const address = document.createElement("p");
+  address.textContent = "123 Salah al-Deen Street, Gaza, Palestine";
+  contactDiv.appendChild(address);
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "Main: (555) 123-4567";
+  contactDiv.appendChild(phoneNumber);
+
+  container.appendChild(contactDiv);
+}
